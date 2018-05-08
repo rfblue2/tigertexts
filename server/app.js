c8bfa8992c7eebaaad9f897de0f3caf0f37783c3
dfan97 <davidfan219@gmail.com>
2018-05-07 20:52:12 -0400

    Simple route for admin dashboard and skeleton UI

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ import Classes from './routes/classes';
 import Books from './routes/books';
 import Listings from './routes/listings';
 import Transactions from './routes/transactions';
+import Admin from './routes/admin';
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.use(sslRedirect([
   'production',
@@ -51,7 +53,7 @@ app.use('/api/classes', Classes);
 app.use('/api/books', Books);
 app.use('/api/listings', Listings);
 app.use('/api/transactions', Transactions);
-
+app.use('/admin', Admin);
 // render react app for anything else
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
