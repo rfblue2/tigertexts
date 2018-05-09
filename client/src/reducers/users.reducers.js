f11d3bc1bd9d38128923599dc696cfd49718fc4b
Roland Fong <rfblue2@gmail.com>
2018-05-09 03:31:34 -0400

    User logging

--- client/src/reducers/users.reducers.js
+++ client/src/reducers/users.reducers.js
@@ -65,6 +65,7 @@ const userReducer = (state = {
     case GET_USER_REQ:
       return { ...state, error: null };
     case GET_USER_RES:
+      console.log("Got user: " + JSON.stringify(user, null, 2));
       return {
         ...state, user: user || defaultuser, error: null,
       };
