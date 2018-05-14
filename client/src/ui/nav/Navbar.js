86c6cb10aa80f3153ea19ef70d2fa44731af0dc3
Roland Fong <rfblue2@gmail.com>
2018-05-14 00:49:19 -0400

    Logout redirects to root

--- client/src/ui/nav/Navbar.js
+++ client/src/ui/nav/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -51,6 +52,8 @@ class Navbar extends Component {
               <Button
                 color="inherit"
                 onClick={handleLogout}
+                component={Link} 
+                to="/" 
                 className={classes.logoutButton}
               >Logout
               </Button> :
